6b5cbf3e582b71ad5c53582264e2698b4d2d25fb
betty2002o <betty2002o@gmail.com>
2021-10-31 22:14:36 -0700

    change time format

--- src/DisplayCurrentTime.js
+++ src/DisplayCurrentTime.js
@@ -29,10 +29,16 @@ function DisplayCurrentTime() {
   }
 
   let currentTimePM = `${months[month]} ${dates} ${days[day]}, 0${pmtime}:${minute} PM`;
+  let passtenPM = `${months[month]} ${dates} ${days[day]}, ${pmtime}:${minute} PM`;
   let currentTimeAM = `${months[month]} ${dates} ${days[day]}, 0${hour}:${minute} AM`;
+  let passtenAM = `${months[month]} ${dates} ${days[day]}, ${hour}:${minute} `;
 
-  if (hour >= 13) {
+  if (hour >= 13 && pmtime > 9) {
+    return <div>{passtenPM}</div>;
+  } else if (hour >= 13) {
     return <div>{currentTimePM}</div>;
+  } else if (hour > 9) {
+    return <div>{passtenAM}</div>;
   } else {
     return <div>{currentTimeAM}</div>;
   }
